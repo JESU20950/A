e5f17c17e0cc2b8a473f008b4159de425783ea06
jesus <jmr_sultan@hotmail.com>
2020-06-11 19:09:43 +0200

    P79936.cc

--- Dynamic-programming/P88567.cc
+++ Dynamic-programming/P88567.cc
@@ -8,23 +8,23 @@ int letras(int n){
 int main(){
   string palabra;
   cin >> palabra;
-  vector<vector<int> > square (palabra.size(),vector <int> (palabra.size()));
-  square[0][0] = letras('A'-palabra[0]+1);
+  vector<vector<int> > square (500,vector <int> (500));
+  square[0][0] = letras(palabra[0]-'A'+1);
   cout << square[0][0];
   for (int i = 1; i<palabra.size(); i++){
-      square[0][i] = square[0][i-1] + letras('A'-palabra[i]+1);
-      cout << square[0][i];
+      square[0][i] = square[0][i-1] + letras(palabra[i]-'A'+1);
+      cout << " " << square[0][i];
   }
   cout << endl;
-  for (int i = 1; i<palabra.size(); ++i){
-      cin >> palabra;
-      square[i][0] = letras('A'-palabra[0]+1) + square[i-1][0];
+  int i = 1;
+  while(cin >> palabra){
+      square[i][0] = letras(palabra[0]-'A'+1) + square[i-1][0];
       cout << square[i][0];
       for (int j = 1; j<palabra.size(); j++){
-        square[i][j] = square[i][j-1] + square[i-1][j] + letras('A'-palabra[j]+1);
-        cout << square[i][j];
+        square[i][j] = (square[i][j-1]-square[i-1][j-1]) + square[i-1][j] + letras(palabra[j]-'A'+1);
+        cout << " " << square[i][j];
       }
       cout << endl;
+      i++;
   }
-    
 }
